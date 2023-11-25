84badac103e01f169e02ba9f5b7b4f3108dd8af2
xwyang457 <112594752+xwyang457@users.noreply.github.com>
2023-11-25 18:58:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,37 @@
-// JavaScript for generating the calendar
-function generateCalendar() {
-    const monthNames = ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"];
+function updateClock() {
     const now = new Date();
-    const month = now.getMonth();
-    const year = now.getFullYear();
-    let daysInMonth = new Date(year, month + 1, 0).getDate();
-
-    let calendarHtml = '<table><tr><th colspan="7">' + monthNames[month] + ' ' + year + '</th></tr>';
-    calendarHtml += '<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr><tr>';
-
-    for (let day = 1; day <= daysInMonth; day++) {
-        let dayOfWeek = new Date(year, month, day).getDay();
-        if (day == 1) {
-            calendarHtml += '<td colspan="' + dayOfWeek + '"></td>';
-        }
-        calendarHtml += '<td>' + day + '</td>';
-        if (dayOfWeek == 6 && day != daysInMonth) {
-            calendarHtml += '</tr><tr>';
-        }
-    }
-    calendarHtml += '</tr></table>';
-    document.querySelector('.calendar').innerHTML = calendarHtml;
+    const utc5 = new Date(now.getTime() + (now.getTimezoneOffset() * 60000) - (5 * 3600000));
+    document.getElementById('time').innerHTML = utc5.toLocaleString();
+    setTimeout(updateClock, 1000);
 }
 
-// JavaScript for adding reminders
-function addReminder() {
-    const reminderText = document.getElementById('reminderText').value;
-    const reminderDate = document.getElementById('reminderDate').value;
-    // Here you can implement storing the reminder in local storage or displaying it on the page
-    console.log('Reminder added:', reminderText, 'on', reminderDate);
-}
-
-// JavaScript for upcoming holidays and countdown
 const holidays = [
-    { name: '新年', date: '2023-01-01' },
-    { name: '国庆节', date: '2023-10-01' }
+    { name: 'Christmas', date: '2023-12-25' },
+    { name: 'New Year', date: '2024-01-01' }
     // Add more holidays as needed
 ];
 
-function displayUpcomingHolidays() {
+function displayHolidays() {
     const today = new Date();
     let htmlContent = '';
-
     for (const holiday of holidays) {
         const holidayDate = new Date(holiday.date);
         const timeDiff = holidayDate - today;
         const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-        htmlContent += '<p>' + holiday.name + ': 还有 ' + daysLeft + ' 天</p>';
+        htmlContent += '<li>' + holiday.name + ': ' + daysLeft + ' days left</li>';
     }
-
-    document.querySelector('.upcoming-holidays').innerHTML = htmlContent;
+    document.getElementById('holidayList').innerHTML = htmlContent;
 }
 
-// Call the functions to generate the calendar and display upcoming holidays on window load
+function addHoliday() {
+    const holidayName = document.getElementById('holidayName').value;
+    const holidayDate = document.getElementById('holidayDate').value;
+    holidays.push({ name: holidayName, date: holidayDate });
+    displayHolidays();
+}
+
 window.onload = function() {
-    generateCalendar();
-    displayUpcomingHolidays();
+    updateClock();
+    displayHolidays();
 };
+
